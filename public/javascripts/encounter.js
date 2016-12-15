1f99272862615290f90e633325a22d9791bb897e
Oliver White <white_oliver@comcast.net>
2016-12-15 11:30:07 -0600

    modified function

--- public/javascripts/encounter.js
+++ public/javascripts/encounter.js
@@ -24,19 +24,6 @@ function getEncounterMultiplier() {
             monsterCount += 1;
         }
     }
-    if (monsterCount === 2) { multiplierIndex = 2; }
-    if (monsterCount > 2 && monsterCount < 7) { multiplierIndex = 3; }
-    if (monsterCount > 6 && monsterCount < 11) { multiplierIndex = 4; }
-    if (monsterCount > 10 && monsterCount < 15) { multiplierIndex = 5; }
-    if (monsterCount >= 15) { multiplierIndex = 6; }
-
-    if (characters.length < 3 && multiplierIndex < multipliers.length - 1) {
-        multiplierIndex += 1;
-    } else if (characters.length > 5 && multiplierIndex > 0) {
-        multiplierIndex -= 1;
-    }
-
-    return multipliers[multiplierIndex];
 }
 
 
