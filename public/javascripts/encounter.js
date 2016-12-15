9bf75c1b35c770b71c0b676530e8c54e10764f23
Oliver White <white_oliver@comcast.net>
2016-12-15 13:17:31 -0600

    added to encounter function

--- public/javascripts/encounter.js
+++ public/javascripts/encounter.js
@@ -1,4 +1,5 @@
 "use strict";
+var characters = [];
 var monsters = [];
 
 //Encounter difficulties 
@@ -24,6 +25,15 @@ function getEncounterMultiplier() {
             monsterCount += 1;
         }
     }
+    if (monsterCount === 2) { multiplierIndex = 2; }
+
+    if (characters.length < 3 && multiplierIndex < multipliers.length - 1) {
+        multiplierIndex += 1;
+    } else if (characters.length > 5 && multiplierIndex > 0) {
+        multiplierIndex -= 1;
+    }
+
+    return multipliers[multiplierIndex];
 }
 
 
