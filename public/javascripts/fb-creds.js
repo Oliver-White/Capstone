8560c6c87a6a7d15cf87344bfc21bdef46a460d4
Oliver White <white_oliver@comcast.net>
2016-12-15 10:34:29 -0600

    moved fb creds out of index

---
+++ public/javascripts/fb-creds.js
@@ -0,0 +1,8 @@
+var config = {
+      apiKey: "AIzaSyBbu2tS-NfzJJlm1KcV5_hIcdl97V3-iVY",
+      authDomain: "creature-selector-ab6c9.firebaseapp.com",
+      databaseURL: "https://creature-selector-ab6c9.firebaseio.com",
+      storageBucket: "creature-selector-ab6c9.appspot.com",
+      messagingSenderId: "297331895164"
+    };
+    firebase.initializeApp(config);
